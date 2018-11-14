0879007a32c9dd359680f4e4fe0c20f86858a1c6
Paula <pcabrapan@gmail.com>
2018-11-14 15:48:25 -0300

    Correciones

--- Codificacion/GUI_Test_Logica/src/mapa/Celda.java
+++ Codificacion/GUI_Test_Logica/src/mapa/Celda.java
@@ -16,62 +16,67 @@ public class Celda {
 	private int y;
 	protected boolean hayObstaculo;
 
-	public Celda(Mapa mapa, int x, int y){
-		entidades=new CopyOnWriteArrayList<Entidad>();
+	public Celda(Mapa mapa, int x, int y) {
+		entidades = new CopyOnWriteArrayList<Entidad>();
 		this.mapa = mapa;
 		this.x = x;
 		this.y = y;
-		hayObstaculo=false;
+		hayObstaculo = false;
 	}
-	
-	public List<Entidad> entidades(){
+
+	public List<Entidad> entidades() {
 		return entidades;
 	}
 
 	public void addEntidad(Entidad e) {
-		entidades.add(e);
-		for (Entidad en : entidades) 
-			en.colision(e);
+		synchronized (entidades) {
+			entidades.add(e);
+			for (Entidad en : entidades)
+				en.colision(e);
+		}
 	}
-	
+
 	public void removeEntidad(Entidad e) {
-		entidades.remove(e);
+		synchronized (entidades) {
+			entidades.remove(e);
+		}
 	}
 
 	public void eliminarEntidad(Entidad e) {
-		entidades.remove(e);
-		entidades.clear();
-		mapa.remove(e);
-		
+		synchronized (entidades) {
+			entidades.remove(e);
+			entidades.clear();
+			mapa.remove(e);
+		}
+
 	}
 
-	public Celda getVecina(int dir){
-		switch (dir){
-			case UP :
-				if (y - 1 >= 0) {
-					return this.mapa.getCelda(x, y - 1);
-				}
-				else {
-					return this.mapa.getCelda(x, mapa.getHeight()-1);
-				}
-			case DOWN :
-				if (y + 1 < mapa.getHeight())
-					return this.mapa.getCelda(x, y + 1);
-				else {
-					return this.mapa.getCelda(x, 0);
-				}
-			case LEFT :
-				if (x - 1 >= 0)
-					return this.mapa.getCelda(x - 1, y);
-				else {
-					return this.mapa.getCelda(mapa.getWidth()-1, y);
-				}
-			case RIGHT :
-				if (x + 1 < mapa.getWidth())
-					return this.mapa.getCelda(x + 1, y);
-				else {
-					return this.mapa.getCelda(mapa.getWidth()-1, y);
-				}
+	public Celda getVecina(int dir) {
+		switch (dir) {
+		case UP:
+			if (y - 1 >= 0) {
+				return this.mapa.getCelda(x, y - 1);
+			} else {
+				return this.mapa.getCelda(x, mapa.getHeight() - 1);
+			}
+		case DOWN:
+			if (y + 1 < mapa.getHeight())
+				return this.mapa.getCelda(x, y + 1);
+			else {
+				return this.mapa.getCelda(x, 0);
+			}
+		case LEFT:
+			if (x - 1 >= 0)
+				return this.mapa.getCelda(x - 1, y);
+			else {
+				return this.mapa.getCelda(mapa.getWidth() - 1, y);
+			}
+		case RIGHT:
+			if (x + 1 < mapa.getWidth())
+				return this.mapa.getCelda(x + 1, y);
+			else {
+				return this.mapa.getCelda(mapa.getWidth() - 1, y);
+			}
 		}
 		return null;
 	}
@@ -85,18 +90,18 @@ public class Celda {
 	}
 
 	public boolean isEndX() {
-		return mapa.getCelda(mapa.getWidth()-1, y) == this;
+		return mapa.getCelda(mapa.getWidth() - 1, y) == this;
 	}
 
 	public boolean isStartX() {
 		return mapa.getCelda(0, y) == this;
 	}
-	
+
 	public void setObstaculo(boolean es) {
 		hayObstaculo = es;
 	}
-	
+
 	public boolean esObstaculo() {
 		return hayObstaculo;
-	}	
+	}
 }
--- Codificacion/GUI_Test_Logica/src/personajes/Entidad.java
+++ Codificacion/GUI_Test_Logica/src/personajes/Entidad.java
@@ -11,52 +11,61 @@ public abstract class Entidad {
 	protected Collider collider;
 	protected int velocidad;
 	protected int hp;
-	
+
 	protected Entidad() {
 		this.grafico = null;
 	}
-	
+
 	protected Entidad(Celda pos) {
 		this.pos = pos;
 		this.grafico = null;
 	}
-	
+
 	protected Entidad(Celda pos, int hp) {
 		this.pos = pos;
 		this.grafico = null;
-		this.hp=hp;
+		this.hp = hp;
 	}
 
 	public Celda getPos() {
 		return pos;
 	}
-	
-	public JLabel getGrafico(){
+
+	public JLabel getGrafico() {
 		return this.grafico.getGrafico();
 	}
-	
-	public void morir(){
-		pos.eliminarEntidad(this);
+
+	public void morir() {
+		synchronized (pos) {
+			pos.eliminarEntidad(this);
+			hp = 0;
+		}
 	}
-	
-	public void mover(int dir){
-		this.pos.removeEntidad(this);
-		Celda next = this.pos.getVecina(dir);
-		this.pos = next;
-		this.grafico.mover(dir);
-		pos.addEntidad(this);
+
+	public void mover(int dir) {
+		synchronized (pos) {
+			this.pos.removeEntidad(this);
+			Celda next = this.pos.getVecina(dir);
+			this.pos = next;
+			this.grafico.mover(dir);
+			pos.addEntidad(this);
+		}
 	}
-	
+
 	public void setPos(Celda c) {
 		pos = c;
-		grafico.setPos(c.getX(),c.getY());
+		grafico.setPos(c.getX(), c.getY());
 		c.addEntidad(this);
 	}
-	
-	public abstract void colision(Entidad e); 
-	
+
+	public boolean isAlive() {
+		return hp > 0;
+	}
+
+	public abstract void colision(Entidad e);
+
 	public abstract void aceptar(Collider c);
-	
+
 	public abstract void mover();
-	
+
 }
